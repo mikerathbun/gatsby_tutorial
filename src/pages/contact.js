7e2d8f9df242e76578e3f681bd821571e8398068
Mike Rathbun <mikerathbun@me.com>
2020-01-13 12:38:18 -0600

    Starting part 4

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout'
+
+export default () => (
+    <Layout>
+        <div>
+            <h1>This is the top of the contact page</h1>
+            <p>
+                <a href="mailto:me@example.com">me@example.com</a>
+            </p>
+        </div>
+    </Layout>
+)
